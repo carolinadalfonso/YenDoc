c5df9c528e6d068bc2a0ca4974de22a8edf9a69f
Carolina DAlfonso <carolina.dalfonso@gmail.com>
2022-09-15 00:07:46 -0300

    Arreglos nombres ✍️

--- lib/data/repositories/authorization_repository.dart
+++ lib/data/repositories/authorization_repository.dart
@@ -3,7 +3,8 @@ import 'package:yendoc/core/errors/failures/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:yendoc/domain/repositories/iauthorization_repository.dart';
 
-import 'base/irepository.dart';
+import '../models/requests/login_model/login_model.dart';
+import 'base/repository.dart';
 
 class AuthorizationRepositoty extends Repository implements IAuthorizationRepository {
   AuthorizationRepositoty({required super.dataSource});
@@ -15,8 +16,14 @@ class AuthorizationRepositoty extends Repository implements IAuthorizationReposi
   }
 
   @override
-  Future<Either<Failure, UserEntity>> login(String username, String password) {
+  Future<Either<Failure, UserEntity>> login(LoginModel loginModel) {
     // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> changePassword(String password, String newPassword) {
+    // TODO: implement changePassword
     throw UnimplementedError();
   }
 }
