dea62ad5fff5d511f8a5331131b1362b8c5d10d5
Carolina DAlfonso <carolina.dalfonso@gmail.com>
2022-11-10 19:52:55 -0300

    Lista de apps para Mapa 🗺️

--- lib/core/framework/util/map.dart
+++ lib/core/framework/util/map.dart
@@ -17,26 +17,92 @@ class MapUtils {
     } else {
       showModalBottomSheet(
         context: context,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        backgroundColor: Colors.white,
         builder: (BuildContext context) {
           return SafeArea(
-            child: SingleChildScrollView(
-              child: Wrap(
-                children: <Widget>[
-                  for (var map in availableMaps)
-                    ListTile(
-                      onTap: () => map.showMarker(
-                        coords: Coords(latitude, longitude),
-                        title: Localization.xMap.patient,
-                      ),
-                      title: Text(map.mapName),
-                      leading: SvgPicture.asset(
-                        map.icon,
-                        height: 30.0,
-                        width: 30.0,
-                      ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width: 100,
+                  height: 6,
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(top: 10, bottom: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[200],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 5),
+                  child: Text(
+                    "Seleccione una aplicación",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                      color: Colors.grey[400],
                     ),
-                ],
-              ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ...availableMaps.asMap().entries.map(
+                        (map) {
+                          return Material(
+                            type: MaterialType.card,
+                            child: InkWell(
+                              onTap: () => map.value.showMarker(
+                                coords: Coords(latitude, longitude),
+                                title: Localization.xMap.patient,
+                              ),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        SvgPicture.asset(
+                                          map.value.icon,
+                                          height: 30.0,
+                                          width: 30.0,
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(left: 10),
+                                          child: Text(
+                                            map.value.mapName,
+                                            style: const TextStyle(
+                                              fontFamily: 'Poppins',
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  if (map.key != availableMaps.length - 1)
+                                    const Divider(
+                                      height: 0,
+                                      indent: 20,
+                                      endIndent: 20,
+                                    ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           );
         },
